69e77132b2b8660ef7dcae64fcd1af275e95bf74
timka654 <relife87@outlook.com>
2019-04-22 10:30:12 +0300

    Добавлены атрибуты для загрузки лобби запросов и менеджеров

---
+++ Utils/Helper/Manager/LobbyServerManagerLoadAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.Helper.Manager
+{
+    public class LobbyServerManagerLoadAttribute : ManagerLoadAttribute
+    {
+        public LobbyServerManagerLoadAttribute(int offset) : base(offset)
+        {
+        }
+    }
+}
