bd913e45970cd84e8c890b1f937676f27f98dad1
relife87 <relife87@outlook.com>
2021-07-21 00:59:27 +0300

    Расширение для взаимодействия с IConfiguration asp.net core

---
+++ ConfigurationEngine.Providers.IConfiguration/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationEngine.Providers.IConfiguration
+{
+    public static class ConfigurationExtensions
+    {
+        public static Microsoft.Extensions.Configuration.IConfiguration BuildConfiguration(this IConfigurationManager conf)
+        {
+            var b = new ConfigurationBuilder();
+
+            b.AddInMemoryCollection(conf.GetAllValues().Select(x => new KeyValuePair<string, string>(x.Path, x.Value)).AsEnumerable());
+
+            return b.Build();
+        }
+    }
+}
