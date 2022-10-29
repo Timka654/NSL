fcfd61f17fb34ba6062b2dca3d3136de803a4799
Timur Meyliev <Timka654work@gmail.com>
2022-10-30 00:11:02 +0300

    remove BinarySerializer missed files
    clear solutions

--- Core/Extensions/SocketCore.Extensions.BinarySerializer/BinaryNetworkTypeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NSL.SocketCore.Extensions.BinarySerializer
-{
-    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
-    public class BinaryNetworkTypeAttribute : Attribute
-    {
-    }
-}
