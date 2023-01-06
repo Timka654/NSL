8bb1adf4cfa114fdc4b51712b1f3afe27f644425
Timur Meyliev <Timka654work@gmail.com>
2023-01-06 22:49:54 +0200

    extension class

---
+++ Core/Utils/NSL.Utils/NSLEndPoint.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace NSL.Utils
+{
+    public class NSLEndPoint
+    {
+        public enum Type
+        {
+            Unknown,
+            TCP,
+            UDP,
+            WS
+        }
+
+        public Type ProtocolType { get; private set; } = Type.Unknown;
+
+        public string EndPoint { get; private set; }
+
+        public string Address { get; private set; }
+
+        public int Port { get; private set; }
+
+        private NSLEndPoint() { }
+
+        public NSLEndPoint(string endPoint)
+        {
+            this.EndPoint = endPoint;
+
+            var uri = new Uri(endPoint);
+
+            ProtocolType = (Type)Enum.Parse(typeof(Type),uri.Scheme, true);
+
+            Address = uri.Host;
+
+            Port = uri.Port;
+        }
+
+        public static NSLEndPoint FromUrl(string url)
+            => new NSLEndPoint(url);
+
+        public static NSLEndPoint FromIPAddress(Type protocolType, IPAddress address, int port)
+            => new NSLEndPoint($"{Enum.GetName(typeof(Type), protocolType)}://{address}:{port}");
+
+        public static NSLEndPoint FromDomain(Type protocolType, string address, int port)
+            => new NSLEndPoint($"{Enum.GetName(typeof(Type), protocolType)}://{address}:{port}");
+
+        public static NSLEndPoint Parse(string endPoint) => new NSLEndPoint(endPoint);
+
+        public override string ToString()
+            => EndPoint;
+    }
+}
