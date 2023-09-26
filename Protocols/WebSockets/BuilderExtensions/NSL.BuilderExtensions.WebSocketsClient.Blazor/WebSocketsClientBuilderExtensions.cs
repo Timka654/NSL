b36d3c9c4b1d9d8399a2ef6deb4821d4f20f0ce6
Timur Meyliev <Timka654work@gmail.com>
2023-09-26 16:50:38 +0300

    wip on wrapper for Blazor(WASM) platform

---
+++ Protocols/WebSockets/BuilderExtensions/NSL.BuilderExtensions.WebSocketsClient.Blazor/WebSocketsClientBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using NSL.SocketClient;
+using NSL.WebSockets.Client;
+using NSL.WebSockets.Client.Blazor;
+
+namespace NSL.BuilderExtensions.WebSocketsClient.Blazor
+{
+    public static class WebSocketsClientBuilderExtensions
+    {
+        public static BlazorWSNetworkClient<TClient, TOptions> BuildForBlazorWASMPlatform<TClient, TOptions>(this WebSocketsClientEndPointBuilder<TClient, TOptions> builder)
+        where TClient : BaseSocketNetworkClient, new()
+        where TOptions : WSClientOptions<TClient>, new()
+        {
+            var result = new BlazorWSNetworkClient<TClient, TOptions>(builder.GetWSClientOptions());
+
+            result.OnReceivePacket += builder.GetReceiveHandles();
+
+            result.OnSendPacket += builder.GetSendHandles();
+
+            return result;
+        }
+    }
+}
---
+++ Protocols/WebSockets/NSL.WebSockets.Client.Blazor/BlazorWSNetworkClient.cs
@@ -0,0 +1,29 @@
+﻿using NSL.SocketClient;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace NSL.WebSockets.Client.Blazor
+{
+    public class BlazorWSNetworkClient<T, TOptions> : WSNetworkClient<T, TOptions>
+        where T : BaseSocketNetworkClient, new()
+        where TOptions : WSClientOptions<T>
+    {
+        public BlazorWSNetworkClient(TOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override WebSocket CreateWS()
+        {
+            return new ClientWebSocket();
+        }
+
+        protected override async Task ConnectAsync(Uri endPoint, CancellationToken cts)
+        {
+            await ((ClientWebSocket)client).ConnectAsync(endPoint, cts);
+        }
+}
+}
