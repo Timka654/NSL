34deab56a46b2678c1da9213ffed749bf9207de5
Timur Meyliev <Timka654work@gmail.com>
2024-04-21 15:39:23 +0300

    wip shared project template

---
+++ Templates/ProjectTemplates/content/shared.combine.library/Class1.Server.cs
@@ -0,0 +1,13 @@
+﻿//-:cnd:noEmit
+#if SERVER
+
+namespace Shared.Combine.Library
+{
+    public partial class Class1
+    {
+
+    }
+}
+
+#endif
+//+:cnd:noEmit
---
+++ Templates/ProjectTemplates/content/shared.combine.library/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Combine.Library
+{
+    public partial class Class1
+    {
+
+    }
+}
