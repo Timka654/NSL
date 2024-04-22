31c71f6133e0b8d57b1de6483695992310f5dac9
Timur Meyliev <Timka654work@gmail.com>
2024-04-22 15:56:36 +0300

    build data cleaner

---
+++ Utils/NSLLibMetaCleaner/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace NSLLibMetaCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args.FirstOrDefault();
+
+            ProcessDirectory(path);
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void ProcessDirectory(string path)
+        {
+            var f =
+            Directory.GetFiles(path);
+
+            if (f.Any(x => x.EndsWith(".csproj")))
+            {
+                var d = Path.Combine(path,"obj");
+
+                if (Directory.Exists(d))
+                    Directory.Delete(d, true);
+                
+                d = Path.Combine(path,"bin");
+
+                if (Directory.Exists(d))
+                    Directory.Delete(d, true);
+            }
+
+            foreach (var item in Directory.GetDirectories(path))
+            {
+                ProcessDirectory(item);
+            }
+        }
+    }
+}
