3ce622461294f4a5ce64a317eecf0e07d8fe9af7
Timur Meyliev <timka654work@gmail.com>
2024-12-28 16:29:29 +0200

    missed

--- Core/Utils/NSL.Utils/CommandLine/CLHandles/CommandReadStateEnum.cs
+++ Core/Utils/NSL.Utils/CommandLine/CLHandles/CommandReadStateEnum.cs
@@ -2,13 +2,31 @@
 {
     public enum CommandReadStateEnum
     {
+        /// <summary>
+        /// Success status for return from a command that executed successfully
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// 
+        /// </summary>
         HelpInvoked,
-        NotFound,
-        NotAPath,
+
+        /// <summary>
+        /// Invalid path for execute command
+        /// </summary>
         InvalidPath,
+
+        /// <summary>
+        /// Invalid argument for execute command, or argument required not found
+        /// </summary>
         InvalidArgument,
+
         FinishPath,
-        //DescriptionInvoked
+
+        /// <summary>
+        /// Status for return from a command that failed to execute
+        /// </summary>
+        Failed
     }
 }
